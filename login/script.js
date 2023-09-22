89367187c9c3b5c3de65c6e50676bf55076b3ef3
Jordano Poletto <90645419+Jordano27@users.noreply.github.com>
2023-09-22 17:12:45 -0300

    atualizaçoes

---
+++ login/script.js
@@ -0,0 +1,12 @@
+
+const irParaLoginButton = document.getElementById('irParaLogin');
+
+irParaLoginButton.addEventListener('click', function () {
+  window.location.href = '../login/login.html';
+});
+
+const irParaCadastroButton = document.getElementById('irParaCadastro');
+
+irParaCadastroButton.addEventListener('click', function () {
+  window.location.href = '../cadastro/index.html';
+});
